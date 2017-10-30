e41e36b0ff480d035c2ef6b1a88c52c4b2cb6c1a
arnaugiro <arnau.giro1@gmail.com>
2017-10-30 23:14:06 +0000

    Ex 3,4,5 - Udemy Course

---
+++ pywk-4.py
@@ -0,0 +1,9 @@
+#a = "1"
+#b = 2
+#print(a + b)
+
+#a should be int, not str
+
+a = "1"
+b = 2
+print(int(a) + b)
---
+++ pywk-5.py
@@ -0,0 +1,4 @@
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print (letters[1])
+
+#Every item of a list is referenced to an index number starting from zero and increasing by one.
